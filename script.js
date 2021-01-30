b87a7a5237e1d01715d7a3c9160e0c533a245559
A.S.M.Hasan Sarker <asmhasansarker@gmail.com>
2021-01-30 12:09:10 +0600

    almost done

--- script.js
+++ script.js
@@ -1,57 +1,160 @@
 const firstClassPlus = document.getElementById("firstClassPlus");
 const firstClassMinus = document.getElementById("firstClassMinus");
-const economyPlus =document.querySelector("#economyPlus");
-const economyMinus = document.querySelector("#economyMinus");
-const bookBtn = document.querySelector("#bookBtn");
-
-firstClassPlus.addEventListener("click",function(){
-    let fTicketAmount = document.querySelector("#firstClassTicket").value;
-    let fTicketNumber = parseInt(fTicketAmount);
-    fTicketNumber++;
-    document.querySelector("#firstClassTicket").value = fTicketNumber;
-    total()
-    
-})
+const economyPlus =document.getElementById("economyPlus");
+const economyMinus = document.getElementById("economyMinus");
+const bookBtn = document.getElementById("bookBtn");
 
 
 
-firstClassMinus.addEventListener("click",function(){
-    let fTicketAmount = document.querySelector("#firstClassTicket").value;
-    let fTicketNumber = parseInt(fTicketAmount);
-    fTicketNumber--;
-    if(fTicketNumber<0){
-        fTicketNumber = 0;
+
+
+function ticketChange(ticketInput ,isIncrease){
+    let fTicketNumber = getInputNumber(ticketInput);
+    if(isIncrease == true){
+        fTicketNumber++;
     }
-    document.querySelector("#firstClassTicket").value = fTicketNumber;
-    total()
-    
-})
-
-economyPlus.addEventListener("click",function(){
-    let eTicketAmount = document.querySelector("#eTicket").value;
-    let eTicketNumber = parseInt(eTicketAmount);
-    eTicketNumber++;
-    document.querySelector("#eTicket").value = eTicketNumber;
-    total()
-    
-})
-
-economyMinus.addEventListener("click",function(){
-    let eTicketAmount = document.querySelector("#eTicket").value;
-    let eTicketNumber = parseInt(eTicketAmount);
-    eTicketNumber--;
-    if(eTicketNumber < 0)
-    {
-        eTicketNumber = 0;
+    if(isIncrease == false && fTicketNumber > 0){
+        fTicketNumber--;
     }
-    document.querySelector("#eTicket").value = eTicketNumber;
-    total()
+    document.getElementById(ticketInput).value = fTicketNumber;
+    total();
+}
+
+
+
+
+
+// function handleFTicketChange(isIncrease){
+//     let fTicketNumber = getInputNumber("firstClassTicket");
+//     if(isIncrease == true){
+//         fTicketNumber++;
+//     }
+//     if(isIncrease == false && fTicketNumber > 0){
+//         fTicketNumber--;
+//     }
+//     document.getElementById("firstClassTicket").value = fTicketNumber;
+//     total();
+// }
+
+
+// function handleETicketChange(isIncrease){
+//     let eTicketNumber = getInputNumber("eTicket");
+//     if(isIncrease == true){
+//         eTicketNumber++;
+//     }
+//     if(isIncrease == false && eTicketNumber > 0){
+//         eTicketNumber--;
+//     }
+//     document.getElementById("eTicket").value = eTicketNumber;
+//     total();
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function handleFTicketChange(isIncrease){
+//     let fTicketNumber = getInputNumber("firstClassTicket");
+//     if(isIncrease == true){
+//         fTicketNumber++;
+//     }
+//     if(isIncrease == false && fTicketNumber > 0){
+//         fTicketNumber--;
+//     }
+//     document.getElementById("firstClassTicket").value = fTicketNumber;
+//     total();
     
-})
+// }
+
+
+// function handleETicketChange(isIncrease){
+//     let eTicketNumber = getInputNumber("eTicket");
+//     if(isIncrease == true){
+//         eTicketNumber++;
+//     }
+//     if(isIncrease == false && eTicketNumber > 0){
+//         eTicketNumber--;
+//     }
+//     document.getElementById("eTicket").value = eTicketNumber;
+//     total();
+// }
+
+
+
+
+
+
+// firstClassPlus.addEventListener("click",function(){
+//     // let fTicketAmount = document.getElementById("firstClassTicket").value;
+//     // let fTicketNumber = parseInt(fTicketAmount);
+//     let fTicketNumber = getInputNumber("firstClassTicket");
+//     fTicketNumber++;
+//     document.getElementById("firstClassTicket").value = fTicketNumber;
+//     total()
+    
+// })
+
+
+
+// firstClassMinus.addEventListener("click",function(){
+//     // let fTicketAmount = document.getElementById("firstClassTicket").value;
+//     // let fTicketNumber = parseInt(fTicketAmount);
+//     let fTicketNumber = getInputNumber("firstClassTicket");
+//     fTicketNumber--;
+//     if(fTicketNumber<0){
+//         fTicketNumber = 0;
+//     }
+//     document.getElementById("firstClassTicket").value = fTicketNumber;
+//     total()
+    
+// })
+
+
+function getInputNumber(id){
+    var amount = document.getElementById(id).value;
+    var number = parseInt(amount);
+    return number;
+}
+
+
+
+
+// economyPlus.addEventListener("click",function(){
+//     // let eTicketAmount = document.getElementById("eTicket").value;
+//     // let eTicketNumber = parseInt(eTicketAmount);
+//     let eTicketNumber = getInputNumber("eTicket");
+//     eTicketNumber++;
+//     document.getElementById("eTicket").value = eTicketNumber;
+//     total()
+    
+// })
+
+// economyMinus.addEventListener("click",function(){
+//     // let eTicketAmount = document.getElementById("eTicket").value;
+//     // let eTicketNumber = parseInt(eTicketAmount);
+//     let eTicketNumber = getInputNumber("eTicket");
+//     eTicketNumber--;
+//     if(eTicketNumber < 0)
+//     {
+//         eTicketNumber = 0;
+//     }
+//     document.getElementById("eTicket").value = eTicketNumber;
+//     total()
+    
+// })
 
 function total(){
-    let fTicketNumber = parseInt(document.querySelector("#firstClassTicket").value);
-    let eTicketNumber = parseInt(document.querySelector("#eTicket").value);
+    let fTicketNumber = getInputNumber("firstClassTicket");
+    let eTicketNumber = getInputNumber("eTicket");
     let subtotal = (fTicketNumber * 150) + (eTicketNumber * 100);
     let vat = subtotal * 0.1;
     vat.toFixed(2);
@@ -59,22 +162,8 @@ function total(){
     document.querySelector("#subtotal").innerText = subtotal;
     document.querySelector("#vat").innerText = vat;
     document.querySelector("#total").innerText = totalAmount; 
-    
-
 }
 
-// bookBtn.addEventListener("click",function(){
-//     let fTicketNumber = parseInt(document.querySelector("#firstClassTicket").value);
-//     let eTicketNumber = parseInt(document.querySelector("#eTicket").value);
-//     document.querySelector("#f").innerText = fTicketNumber;
-//     document.querySelector("#e").innerText = eTicketNumber;
-//     let head = document.querySelector("#head");
-//     let main = document.querySelector("#main");
-//     head.classList.add = "custom-gray";
-//     main.classList.add = "custom-gray";
-//     let success = document.querySelector("#congratulation-area");
-//     success.style.display = "block";
-// })
 
 
 
@@ -83,8 +172,8 @@ function total(){
 // ---------------------
 
 function popup(){
-    let fTicketNumber = parseInt(document.querySelector("#firstClassTicket").value);
-    let eTicketNumber = parseInt(document.querySelector("#eTicket").value);
+    let fTicketNumber = getInputNumber("firstClassTicket");
+    let eTicketNumber = getInputNumber("eTicket");
     document.querySelector("#f").innerText = fTicketNumber;
     document.querySelector("#e").innerText = eTicketNumber;
     document.getElementById("popup-1").classList.add("active");
